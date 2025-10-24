fc7367b1b907658d7b5c0d8832f9fcf62f0635a1
Paulo-VictorSB <paulo.vdbarbosa@gmail.com>
2025-10-24 16:31:36 -0300

    estudos sobre template

--- meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -10,8 +11,18 @@ export class TemplateBinding {
   public name: string = "Paulo Victor";
   public age: number = 20;
   public isDisable: Boolean = true
+  public plus: string = "";
+  public valueXandY = "";
 
-  public sum(n1: number, n2:number) {
+  public sum(n1: number, n2: number) {
     return n1 + n2;
+  }
+
+  public addPlus() {
+    this.plus = this.plus + "*"
+  }
+
+  public position(event: MouseEvent){
+    this.valueXandY = `X: ${event.clientX}` + " " + `Y: ${event.clientY}`;
   }
 }
