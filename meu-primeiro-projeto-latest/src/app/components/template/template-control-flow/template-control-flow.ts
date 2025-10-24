a5bcd0a83f221f7f2aa5dfd7f224a28d2c95d756
Paulo <paulo.vdbarbosa@gmail.com>
2025-10-24 19:34:33 -0300

    control-flow @for()

--- meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-template-control-flow',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './template-control-flow.html',
   styleUrl: './template-control-flow.scss',
 })
 export class TemplateControlFlow {
   public isTrue: Boolean = true;
+
+  public loadingData$: Observable<string[]> = of ([
+    'item 1',
+    'item 2',
+    'item 3',
+  ]).pipe(delay(3000));
 }
