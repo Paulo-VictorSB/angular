7791a18e47673e4482d5181bd0da413865617250
Paulo-VictorSB <paulo.vdbarbosa@gmail.com>
2025-10-24 14:19:24 -0300

    atributes and properties bidings

--- meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 export class TemplateBinding {
   public name: string = "Paulo Victor";
   public age: number = 20;
+  public isDisable: Boolean = true
 
   public sum(n1: number, n2:number) {
     return n1 + n2;
