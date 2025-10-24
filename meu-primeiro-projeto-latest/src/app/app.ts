f6654e2433d8afe1880e374e680691d43b96237b
Paulo-VictorSB <paulo.vdbarbosa@gmail.com>
2025-10-24 13:39:56 -0300

    text interpolation

--- meu-primeiro-projeto-latest/src/app/app.ts
+++ meu-primeiro-projeto-latest/src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NewComponent } from './components/new-component/new-component';
+import { TemplateBinding } from './components/template/template-binding/template-binding';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NewComponent],
+  imports: [RouterOutlet, TemplateBinding],
   template: `
     <router-outlet />
     <h1>Curso de Angular</h1>
-    <app-new-component />
+    <app-template-binding/>
   `
 })
 export class App {
---
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-binding',
+  imports: [],
+  templateUrl: './template-binding.html',
+  styleUrl: './template-binding.scss',
+})
+export class TemplateBinding {
+  public name: string = "Paulo Victor";
+  public age: number = 20;
+
+  public sum(n1: number, n2:number) {
+    return n1 + n2;
+  }
+}
