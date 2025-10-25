f56fc0217f5eaaa94ca8e933c18003cc9fad44e6
Paulo <paulo.vdbarbosa@gmail.com>
2025-10-25 15:55:30 -0300

    aprendendo sobre @deferrable views

--- meu-primeiro-projeto-latest/src/app/app.ts
+++ meu-primeiro-projeto-latest/src/app/app.ts
@@ -1,14 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TemplateControlFlow } from './components/template/template-control-flow/template-control-flow';
+import { TemplateDeferrableViews } from './components/template/template-deferrable-views/template-deferrable-views';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TemplateControlFlow],
+  imports: [RouterOutlet, TemplateDeferrableViews],
   template: `
     <router-outlet />
     <h1>Curso de Angular</h1>
-    <app-template-control-flow />
+    <app-template-deferrable-views />
   `
 })
 export class App {
---
+++ meu-primeiro-projeto-latest/src/app/components/template/template-deferrable-views/template-deferrable-views.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NewComponent } from '../../new-component/new-component';
+
+@Component({
+  selector: 'app-template-deferrable-views',
+  imports: [NewComponent],
+  templateUrl: './template-deferrable-views.html',
+  styleUrl: './template-deferrable-views.scss',
+})
+export class TemplateDeferrableViews {
+
+}
