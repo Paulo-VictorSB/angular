59cd74ae1df187ae289d1e7c5665d6827d1a3279
Paulo-VictorSB <paulo.vdbarbosa@gmail.com>
2025-10-27 16:31:20 -0300

    projeto-portifolio

--- projeto-portifolio/src/app/modules/portifolio/components/projects/projects.ts
+++ projeto-portifolio/src/app/modules/portifolio/components/projects/projects.ts
@@ -1,13 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/IProjects.interface';
+
+// Material
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+
+// Enum
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+
+// Dialog
+import { DialogProjects } from '../dialog/dialog-projects/dialog-projects';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
 export class Projects {
+  #dialog = inject(MatDialog);
   public arrayProjects = signal<IProjects[]>([
     {
       src: "assets/img/projects/Encurtei.png",
@@ -15,13 +25,20 @@ export class Projects {
       title: "Encurtei",
       width: "100px",
       height: "100px",
-      description: "<p>Um sistema de encurtamento de links 100% online e gratuito</p>",
+      description: "<p>O Encurtei é a melhor ferramenta gratuita para encurtar URLs no Brasil. Transforme links longos em URLs curtas, seguras e fáceis de compartilhar em redes sociais, WhatsApp, email e qualquer lugar.</p> <p>Um encurtador de URLs é uma ferramenta essencial para quem precisa compartilhar links de forma eficiente. O Encurtei oferece a melhor solução gratuita para transformar URLs longas em links curtos e memoráveis.</p>",
       links: [
         {
           name: "Conheça o projeto",
           href: "https://encurtei.online"
         },
       ]
-    }
+    },
   ])
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjects, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    })
+  }
 }
