67a9c8550ee7a0be3a3859cecf195fd69dfbbfe1
Paulo-VictorSB <paulo.vdbarbosa@gmail.com>
2025-11-06 09:43:05 -0300

    template-driven-forms

--- meu-primeiro-projeto-latest/src/app/app.ts
+++ meu-primeiro-projeto-latest/src/app/app.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AngularPipes } from './components/pipes/angular-pipes/angular-pipes';
+import { TemplateDrivenForms } from './components/forms/template-driven-forms/template-driven-forms';
+import { ReactiveForms } from './components/forms/reactive-forms/reactive-forms';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AngularPipes],
+  imports: [RouterOutlet, TemplateDrivenForms, ReactiveForms],
   template: `
     <router-outlet />
     <h1>Curso de Angular</h1>
-    <app-angular-pipes/>
+    <app-template-driven-forms />
+    <app-reactive-forms />
   `
 })
 export class App {
