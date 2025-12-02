7adcc961244da5ebe69c2a982798379ec69edf77
Paulo-VictorSB <paulo.vdbarbosa@gmail.com>
2025-12-02 14:10:47 -0300

    Reactive forms - Form Groups e Form Controls

--- meu-primeiro-projeto-latest/src/app/app.ts
+++ meu-primeiro-projeto-latest/src/app/app.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TemplateDrivenForms } from './components/forms/template-driven-forms/template-driven-forms';
 import { ReactiveForms } from './components/forms/reactive-forms/reactive-forms';
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TemplateDrivenForms, ReactiveForms],
+  imports: [RouterOutlet, ReactiveForms],
   template: `
     <router-outlet />
     <h1>Curso de Angular</h1>
-    <app-template-driven-forms />
     <app-reactive-forms />
   `
 })
+
 export class App {
   title = 'meu-primeiro-projeto-latest';
 }
--- meu-primeiro-projeto-latest/src/app/components/forms/reactive-forms/reactive-forms.ts
+++ meu-primeiro-projeto-latest/src/app/components/forms/reactive-forms/reactive-forms.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-forms.html',
   styleUrl: './reactive-forms.scss',
 })
 export class ReactiveForms {
+  public profileForm = new FormGroup({
+    name: new FormControl('Paulo Victor'),
+    myStacks: new FormGroup({
+      front: new FormControl('Angular'),
+      back: new FormControl('Php')
+    })
+  })
 
+  public update()
+  {
+    this.profileForm.patchValue({
+      name: "Paulo Victor Atualizado",
+      myStacks: {
+        front: "Angular Atualizado",
+        back: "Php Atualizado"
+      },
+    })
+  }
 }
