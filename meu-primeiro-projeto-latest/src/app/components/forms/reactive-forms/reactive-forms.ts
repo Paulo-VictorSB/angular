47b9a0cef5cad7e4589b83915ca645d2e06714b1
Paulo-VictorSB <paulo.vdbarbosa@gmail.com>
2025-12-03 09:16:48 -0300

    Reactive forms - form validators e form acessors

--- meu-primeiro-projeto-latest/src/app/components/forms/reactive-forms/reactive-forms.ts
+++ meu-primeiro-projeto-latest/src/app/components/forms/reactive-forms/reactive-forms.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './reactive-forms.html',
   styleUrl: './reactive-forms.scss',
 })
@@ -11,10 +12,10 @@ export class ReactiveForms {
   #fb = inject(FormBuilder);
 
   public profileForm = this.#fb.group({
-    name: ['Paulo Victor'],
+    name: ['', Validators.minLength(5)],
     myStacks: this.#fb.group({
-      front: ['Angular'],
-      back: ['Php']
+      front: [''],
+      back: ['']
     }),
     myFavoriteFoods: this.#fb.array([['X-Tudo']]),
   })
